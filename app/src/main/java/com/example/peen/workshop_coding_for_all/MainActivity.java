52d77b8d57d87992b03e6b8f68b4f4d3f9271e1b
leoberus <ptrp.srirak@gmail.com>
2017-06-29 00:32:08 +0700

    Request Permission Request Runtime Permission Call Camera Set exist image feed from camera

--- app/src/main/java/com/example/peen/workshop_coding_for_all/MainActivity.java
+++ app/src/main/java/com/example/peen/workshop_coding_for_all/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.peen.workshop_coding_for_all;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.inthecheesefactory.thecheeselibrary.widget.AdjustableImageButton;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private Uri imageUri;
+    private static final int TAKE_PICTURE = 1;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView img_feed = (ImageView) findViewById(R.id.img_feed);
-        Bitmap imagenAndroid = BitmapFactory.decodeResource(getResources(),R.drawable.user);
+        Bitmap imagenAndroid = BitmapFactory.decodeResource(getResources(), R.drawable.user);
         img_feed.setImageBitmap(imagenAndroid);
 
         TextView caption = (TextView) findViewById(R.id.caption);
@@ -27,7 +50,82 @@ public class MainActivity extends AppCompatActivity {
         ImageView img_feed2 = (ImageView) findViewById(R.id.img_feed2);
         img_feed2.setImageBitmap(imagenAndroid);
 
+        AdjustableImageButton Camera_btn = (AdjustableImageButton) findViewById(R.id.camera_btn);
 
 
+
+        Camera_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request_permission();
+            }
+        });
+
+
+    }
+
+    private void request_permission() {
+        int hasWritePermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int hasReadPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasWritePermission != PackageManager.PERMISSION_GRANTED && hasReadPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }else{
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photo));
+                    imageUri = Uri.fromFile(photo);
+                    startActivityForResult(intent, TAKE_PICTURE);
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ImageView imageView = (ImageView) findViewById(R.id.img_feed);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+
+                        imageView.setImageBitmap(bitmap);
+                        Toast.makeText(this, selectedImage.toString(),
+                                Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
     }
 }
